c0cafe8742f099ced12649c2ee16d8d75858f736
princ3raj <princ3raj1999@gmail.com>
2021-04-01 17:13:14 +0530

    path changed

--- Desktop/ApiNino/api/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.urls import path
-from .views import NinoRoomsAPIView
-
-
-
-urlpatterns = [
-path('', NinoRoomsAPIView.as_view()),
-]
--- Desktop/ApiNino/ninorooms/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import NinoRooms
-
-
-class NinoCreateView(forms.ModelForm):
-    class Meta:
-        model = NinoRooms
-        fields = '__all__'
